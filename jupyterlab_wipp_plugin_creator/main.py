7f38a6244d8ffdccdf02df32b05d7da4a70dadf8
Kingstonshaw <will.xiao@axleinfo.com>
2021-10-28 23:34:03 -0700

    feat: demo add file size statistics in the input folder

--- jupyterlab_wipp_plugin_creator/main.py
+++ jupyterlab_wipp_plugin_creator/main.py
@@ -1,8 +1,10 @@
 import os
+from time import ctime
 from pathlib import Path
-import numpy as np
 import argparse
 import logging
+
+import numpy as np
 
 def main():
     
@@ -24,13 +26,36 @@ def main():
     
     script_path = Path(__file__).parent
 
-    logger.info('Arguments:')
     logger.info(f'script path:{script_path}')
+    
+    logger.info('Arguments:')
     logger.info(f'Input collection: {args.input_collection}')
     logger.info(f'Configuration parameter: {args.config_param}')
     logger.info(f'Output collection: {args.output_collection}')
     logger.info(f'Random number generated with numpy: {np.random.rand()*args.config_param}')
+    allFiles = os.listdir(args.input_collection)
 
+# statistics on files present in args.input_collection which should automatically be the full path
+
+    fileSizeArr = []
+    measure = 'MB'
+
+    logger.info(f'Files information in "{args.input_collection}":')
+    for filepath in allFiles:
+        split_tup = os.path.splitext(filepath)
+        filepath = os.path.join(args.input_collection, filepath)
+        fileSize = os.path.getsize(filepath)//(1024*1024)
+        fileSizeArr.append(fileSize)
+        logger.info(f'name:{split_tup[0]}, type: {split_tup[1]}, size: {fileSize} MB, created: {ctime(os.path.getmtime(filepath))}, last modified: {ctime(os.path.getctime(filepath))}')
+    fileSizeArr.sort()
+    collectionSize = sum(fileSizeArr)
+
+    if collectionSize >= 1024:
+        collectionSize = collectionSize//1024
+        measure = 'GB'
+
+    logger.info(f'Collection size is {collectionSize} {measure}. Average file size: {collectionSize//len(fileSizeArr)} MB, Max file size: {fileSizeArr[-1]} MB, Min file size: {fileSizeArr[0]} MB, Median file size: {fileSizeArr[len(fileSizeArr)//2]} MB')
+        # logger.info(f'name:{split_tup[0]} type: {Path(file).suffix}, size:{os.path.getsize(file)}, creation time:{os.path.getmtime(file)}, modification time:{os.path.getctime(file)}')
 
 if __name__ == "__main__":
     main()
